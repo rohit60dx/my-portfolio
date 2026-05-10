66c69a99dd0ae620a066512e0fbf862bab364db9
rohit60dx <rohit50dx@gmail.com>
2026-05-10 15:12:35 +0530

    remove app-store-scraper from server

--- portfolio-api/server.js
+++ portfolio-api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const gplay = require('google-play-scraper');
-const store = require('app-store-scraper');
+
 
 const app = express();
 app.use(cors({ origin: '*' }));
